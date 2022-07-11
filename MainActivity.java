a015bd7ed168962a5c2e8974244c1364585c87f5
shinsam <39876724+shinsam@users.noreply.github.com>
2022-07-11 11:23:35 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+    }
+
+
+    
+}
